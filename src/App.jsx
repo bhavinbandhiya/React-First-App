9c12424d14192d553cd251dfd9ee7f75217dfbae
bhavinbandhiya <bhavinbandhiya003@gmail.com>
2021-07-09 16:59:03 +0530

    Component Life Cycle Metods

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import Bhavin from"./components/user";
+import User from"./components/user";
 //JSX
 function App() {
   return (
   <div>
     <h1>User</h1>
-    <Bhavin />
+    <User name="Bhavin" description="FSWD" />
   </div>
   );
 };
 
 export default App;
+
+// Props - A data that is being passed from one component to another.
+// state - A set of data that an individual component holds.
--- src/components/user.jsx
+++ src/components/user.jsx
@@ -1,10 +1,51 @@
-const User = (props) => {
-  return (
-    <div>
-      <h1>{props.name}</h1>
-      <p>{props.description}</p>
-    </div>
-  );
-};
+import React from "react";
+
+class User extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      planet: "earth",
+    };
+    console.log("Hey I'm fromConstructor");
+  }
+
+  componentDidMount(){
+    this.setState({planet: "Jupiter"});
+  }
+
+// static getDerivedFromProps(prop,state){
+//   console.log("Hey I'm from drived state from props");
+//   return {planet:"jupiter"};
+// }
+
+shouldComponentUpdate(nextProp, nextState){
+  console.log({
+    nextProp,
+    nextState,
+  });
+  return true;
+}
+
+getSnapshotBeforeUpdate(prevProp, prevState){
+  console.log({prevProp, prevState});
+  return true;
+}
+
+componentDidUpdate(){
+  console.log(this.state);
+}
+
+  render() {
+    console.log("Hey I'm from render()");
+    return (
+      <div>
+        <h1>{this.props.name}</h1>
+        <p>{this.props.description}</p>
+        <h4>{this.state.planet}</h4>
+      </div>
+    );
+  }
+}
 
 export default User;
